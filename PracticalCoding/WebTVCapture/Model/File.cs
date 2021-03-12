555d572f753fcf707671456f1fe68130a9dd6c8a
bonghoe <wantrnqhdghl@naver.com>
2021-03-12 17:55:51 +0900

    1. WebTVCapture  프로젝트 추가.

---
+++ PracticalCoding/WebTVCapture/Model/File.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebTVCapture.Model
+{
+    public class File
+    {
+
+        public File()
+        {
+
+        }
+
+        public string FileName { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
---
+++ PracticalCoding/WebTVCapture/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace WebTVCapture.ViewModel
+{
+    public class ViewModelLocator
+    {
+
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<MainWindowViewModel>();
+
+
+
+        }
+
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                return SimpleIoc.Default.GetInstance<MainWindowViewModel>();
+            }
+        }
+    }
+}
