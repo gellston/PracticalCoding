da44034d6b81548a87f9c354f42d7e4244e45b6c
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2021-11-10 17:25:18 +0900

    MVVM Basic Update

---
+++ Source/MVVMBasic/MVVMBasic/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MVVMBasic.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVMBasic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            this.Services = ConfigureServices();
+            this.InitializeComponent();
+
+        }
+
+        public new static App Current => (App)Application.Current;
+
+
+        public IServiceProvider Services { get; }/// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<MainWindowViewModel>();
+
+            return services.BuildServiceProvider(); // IOC 컨테이너 리턴 
+        }
+    }
+}
---
+++ Source/MVVMBasic/MVVMBasic/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMBasic.ViewModel
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+
+        public MainWindowViewModel()
+        {
+            System.Console.WriteLine("test");
+        }
+
+        private string _Text = "";
+        public string Text
+        {
+            get
+            {
+                return _Text;
+            }
+            set
+            {
+                //_Text = value;
+                SetProperty(ref _Text, value);
+            }
+        }
+
+        private bool _check = false;
+        public ICommand HitTestCommand
+        {
+            get => new RelayCommand(() =>
+            {
+                _check = !_check;
+                if (_check == true)
+                    this.Text = "check!!";
+                else
+                    this.Text = "non check!!";
+            });
+        }
+
+    }
+}
