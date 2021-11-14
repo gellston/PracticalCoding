e09453d6637975882db8d4cba719133db56417cd
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2021-11-14 17:21:09 +0900

    Style Example 업데이트

---
+++ Source/MVVMBasic-Style/MVVMBasic/View/MainWindowView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using MVVMBasic.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace MVVMBasic.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        //WPF 의존 프로퍼티
+        public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register("IsOpen", typeof(bool), typeof(MainWindowView));
+        public bool IsOpen
+        {
+            get
+            {
+                return (bool)GetValue(IsOpenProperty);
+            }
+
+            set
+            {
+                SetValue(IsOpenProperty, value);
+            }
+        }
+
+
+     
+    }
+}
---
+++ Source/MVVMBasic-Style/MVVMBasic/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMBasic.ViewModel
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+
+
+        public ICommand OpenMenuCommand
+        {
+            get => new RelayCommand(() =>
+            {
+                this.MenuOpen = !this.MenuOpen;
+            });
+        }
+        
+        //C# Property!!!!!!!!!!!!!
+        private bool _MenuOpen = false;
+        public bool MenuOpen
+        {
+            get => _MenuOpen;
+            set => SetProperty(ref _MenuOpen, value);
+        }
+
+    }
+}
