220a6c95645ccadda5c992f0723cf36ece742f8d
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2021-11-16 15:56:44 +0900

    mvvm basic list view exmaple 업로드

---
+++ Source/MVVMBasic-List/MVVMBasic/Model/Student.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMBasic.Model
+{
+    public class Student : ObservableObject
+    {
+
+        public Student()
+        {
+
+        }
+
+        private string _Name = "";
+        public string Name
+        {
+            get => _Name;
+            set => SetProperty(ref _Name, value);   
+        }
+
+
+
+        private int _Age = 0;
+        public int Age
+        {
+            get => _Age;
+            set => SetProperty(ref _Age, value);
+        }
+
+        public ICommand HitTestCommand
+        {
+            get => new RelayCommand(() =>
+            {
+                System.Diagnostics.Debug.WriteLine("test");
+            });
+        }
+    }
+}
