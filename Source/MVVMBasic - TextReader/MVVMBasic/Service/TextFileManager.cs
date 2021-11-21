b05a199096dbc3e7ead5fa0f729f009137c94b0b
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2021-11-21 15:40:49 +0900

    TextReader 예제 업데이트

---
+++ Source/MVVMBasic - TextReader/MVVMBasic/Service/TextFileManager.cs
@@ -0,0 +1,41 @@
+﻿using MVVMBasic.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMBasic.Service
+{
+    public class TextFileManager
+    {
+
+        public TextFileManager()
+        {
+
+        }
+
+        public ObservableCollection<TextFile> TextFilesReader(string _path)
+        {
+            ObservableCollection<TextFile> collection = new ObservableCollection<TextFile>();
+
+            var files = Helper.FileSystemHelper.GetFiles(_path, "*.txt");
+
+
+            foreach (var file in files)
+            {
+                var textfileModel = new TextFile()
+                {
+                    FileName = Path.GetFileName(file),
+                    FilePath = file
+                };
+                collection.Add(textfileModel);
+            }
+
+
+            return collection;
+        }
+    }
+}
---
+++ Source/MVVMBasic - TextReader/MVVMBasic/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MVVMBasic.ViewModel
+{
+    public class ViewModelLocator
+    {
+
+        public ViewModelLocator()
+        {
+            System.Console.WriteLine("Test");
+        }
+
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get => App.Current.Services.GetService<MainWindowViewModel>();
+        }
+    }
+}
