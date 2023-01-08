acad1742e4be3317c8987a88d22a1365bc5b2c93
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-01-08 15:55:02 +0900

    test

---
+++ Source/WPFMVVMExample1/WPFMVVMExample1/Model/TextFileModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVMExample1.Model
+{
+    public class TextFileModel :  ModelBase
+    {
+
+        #region Private Property
+        private string _FileName = "";
+        private string _FilePath = "";
+
+        #endregion
+
+
+        #region Constructor
+        public TextFileModel() {
+        
+        
+        }
+        #endregion
+
+        #region Public Property
+        public string FileName
+        {
+            get => _FileName;
+            set
+            {
+                _FileName = value;
+                this.OnPropertyChanged(nameof(FileName));
+            }
+        }
+
+        public string FilePath
+        {
+            get => _FilePath;
+            set
+            {
+                _FilePath = value;
+                this.OnPropertyChanged(nameof(FilePath));
+            }
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ Source/WPFMVVMExample1/WPFMVVMExample1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPFMVVMExample1.Command;
+
+namespace WPFMVVMExample1.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        #region Private Property
+       
+        #endregion
+
+        #region Contrsuctor
+        public MainWindowViewModel() { 
+        
+
+
+        
+        }
+        #endregion
+
+
+        #region Collection
+
+        #endregion
+
+        #region Command
+        public ICommand LoadCommand
+        {
+            get => new RelayCommand<object>((objecct) =>
+            {
+
+            });
+        }
+
+        public ICommand SaveCommand
+        {
+            get => new RelayCommand<object>((objecct) =>
+            {
+
+            });
+        }
+        #endregion
+
+
+    }
+}
