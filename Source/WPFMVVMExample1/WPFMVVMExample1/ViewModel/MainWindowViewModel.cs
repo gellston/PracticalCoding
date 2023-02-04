f7bfad6df059b81befd19999dac659aaa3f7e14b
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-02-04 16:04:20 +0900

    Text

--- Source/WPFMVVMExample1/WPFMVVMExample1/ViewModel/MainWindowViewModel.cs
+++ Source/WPFMVVMExample1/WPFMVVMExample1/ViewModel/MainWindowViewModel.cs
@@ -17,6 +17,9 @@ namespace WPFMVVMExample1.ViewModel
 
         #region Private Property
         private ObservableCollection<TextFileModel> _TextFileModelCollection = new ObservableCollection<TextFileModel>();
+
+
+        private TextFileModel _SelectedTextFileModel = null;
         #endregion
 
 
@@ -49,7 +52,15 @@ namespace WPFMVVMExample1.ViewModel
 
 
         #region Public Property
-
+        public TextFileModel SelectedTextFileModel
+        {
+            get => _SelectedTextFileModel;
+            set
+            {
+                _SelectedTextFileModel = value;
+                OnPropertyChanged("SelectedTextFileModel");
+            }
+        }
         #endregion
 
 
@@ -65,5 +76,26 @@ namespace WPFMVVMExample1.ViewModel
         }
         #endregion
 
+
+        #region Command
+        public ICommand OpenFolderCommand
+        {
+            get => new RelayCommand<object>((arg) =>
+            {
+
+
+                System.Diagnostics.Debug.WriteLine("test1");
+
+            });
+        }
+
+        public ICommand SaveFileCommand
+        {
+            get => new RelayCommand<object>((arg) =>
+            {
+                System.Diagnostics.Debug.WriteLine("test2");
+            });
+        }
+        #endregion
     }
 }
