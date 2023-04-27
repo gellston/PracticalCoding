d3c74406cf8b5934f2d0c59bd00efbdf7fe4f5a5
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-04-27 13:58:44 +0900

    source code update

---
+++ Source/C#/WPFResourceExample/WPFResourceExample/Data/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFResourceExample.Data
+{
+    public class Student
+    {
+
+        public string Name { get; set; } = "";
+
+        public int Age { get; set; } = 33;
+
+        public string Address { get; set; } = "There is no cow level";
+
+    }
+}
