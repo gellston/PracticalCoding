9047809a404dd2f9c7b3ec9829ba0fb82da27a12
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-05-13 16:05:40 +0900

    WPFBindingExample update

---
+++ Source/C#/WPFBindingExample/WPFBindingExample/BindingObject.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace WPFBindingExample
+{
+    public class BindingObject : INotifyPropertyChanged
+    {
+
+        #region Private Property
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string _Temp = "";
+        private ListCollectionView _ListBoxItemCollection = null;
+        private ListCollectionView _ListViewItemCollection = null;
+        private ListCollectionView _GridViewItemCollection = null;
+
+        #endregion
+
+        #region Constructor
+        public BindingObject()
+        {
+
+            var people = new List<Person>();
+            people.Add(new Person()
+            {
+                Name = "BongHoe Koo",
+                Age = 33
+            });
+            people.Add(new Person()
+            {
+                Name = "BongHoe Koo",
+                Age = 33
+            });
+            people.Add(new Person()
+            {
+                Name = "BongHoe Koo",
+                Age = 33
+            });
+
+            this.ListViewItemCollection = new ListCollectionView(people);
+            this.ListBoxItemCollection = new ListCollectionView(people);
+            this.GridViewItemCollection = new ListCollectionView(people);
+
+
+        }
+        #endregion
+
+        #region Public Property
+        public string Temp
+        {
+            get => _Temp;
+            set
+            {
+                _Temp = value;
+                OnPropertyChanged(nameof(Temp));
+            }
+        }
+        #endregion
+
+        #region Functions
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Collection
+        public ListCollectionView ListBoxItemCollection
+        {
+            get => _ListBoxItemCollection;
+            set
+            {
+                _ListBoxItemCollection = value;
+                OnPropertyChanged(nameof(ListBoxItemCollection));
+            }
+        }
+
+        public ListCollectionView ListViewItemCollection
+        {
+            get => _ListViewItemCollection;
+            set
+            {
+                _ListViewItemCollection = value;
+                OnPropertyChanged(nameof(ListViewItemCollection));
+            }
+        }
+
+
+        public ListCollectionView GridViewItemCollection
+        {
+            get => _GridViewItemCollection;
+            set
+            {
+                _GridViewItemCollection = value;
+                OnPropertyChanged(nameof(GridViewItemCollection));
+            }
+        }
+
+
+        #endregion
+
+        #region Command
+
+        public ICommand TestCommand
+        {
+            get => new RelayCommand((arg) =>
+            {
+                var view = CollectionViewSource.GetDefaultView(this.ListViewItemCollection) as ListCollectionView;
+                view.MoveCurrentToFirst();
+                if (view != null)
+                {
+                    while (view.CurrentPosition != view.Count)
+                    {
+                        var person = view.CurrentItem as Person;
+                        person.Name = "Steve!";
+                        view.MoveCurrentToNext();
+                    }
+                }
+            });
+        }
+        #endregion
+
+
+
+
+
+    }
+}
---
+++ Source/C#/WPFBindingExample/WPFBindingExample/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFBindingExample
+{
+    public class Person : INotifyPropertyChanged
+    {
+
+        #region Private Property
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _Name = "";
+        private int _Age = 0;
+        #endregion
+
+        #region Functions
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+        #region Public Property
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                _Name = value;  
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public int Age
+        {
+            get => _Age;
+            set
+            {
+                _Age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+
+        #endregion
+
+
+
+    }
+}
