d91419457d19e1e267a3aa37c86356c55820d04d
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-05-31 13:31:57 +0900

    WPF ItemsControlTemplate Example 업데이트

---
+++ Source/C#/WPFItemsControlTemplateExample/WPFItemsControlTemplateExample/Model/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFItemsControlTemplateExample.Model
+{
+    public class Student
+    {
+        public Student() { 
+        
+        }
+
+        public string Name { get; set; } = "";
+        public int Age { get; set; } = 25;
+    }
+}
