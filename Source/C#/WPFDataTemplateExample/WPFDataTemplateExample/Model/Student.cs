528c83b8e3fa785ef43737b272efc01d3460ff71
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-05-31 13:04:47 +0900

    WPFDataTemplateExample 업데이트

---
+++ Source/C#/WPFDataTemplateExample/WPFDataTemplateExample/Model/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDataTemplateExample.Model
+{
+    public class Student
+    {
+
+        public Student() { }
+
+        public string Name { get; set; } = "";
+        public int Age { get; set; } = 25;
+        
+    }
+}
---
+++ Source/C#/WPFDataTemplateExample/WPFDataTemplateExample/Model/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDataTemplateExample.Model
+{
+    public class Teacher
+    {
+        public Teacher() { }
+
+        public string Name { get; set; } = "";
+        public int Age { get; set; } = 25;
+        public string ClassName { get; set; } = "";
+    }
+}
