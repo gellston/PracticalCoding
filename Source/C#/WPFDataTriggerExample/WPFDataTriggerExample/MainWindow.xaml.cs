4de05afe599f19fb3e712ef51f7edfc8574d4ca0
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-06-02 10:31:22 +0900

    DataTrigger Example 업데이트

---
+++ Source/C#/WPFDataTriggerExample/WPFDataTriggerExample/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDataTriggerExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Student student;
+
+        public MainWindow()
+        {
+
+            this.DataContext = student = new Student()
+            {
+                Name = "BongHoe Koo",
+                Age = 18,
+                Bad = false
+            };
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.student.Bad = !this.student.Bad;
+        }
+    }
+}
---
+++ Source/C#/WPFDataTriggerExample/WPFDataTriggerExample/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDataTriggerExample
+{
+    public class Student : INotifyPropertyChanged
+    {
+
+        #region Private Property
+        private string _Name = "";
+        private int _Age = 0;
+        private bool _Bad = false;
+        #endregion
+
+        #region Event
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnRaisePropertyChanged(string propertyName)
+        {
+            if(this.PropertyChanged != null)
+                this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Constructor
+        public Student()
+        {
+
+        }
+
+
+        #endregion
+
+        #region Public Property
+
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                _Name = value;
+                OnRaisePropertyChanged(nameof(Name));
+            }
+        }
+
+        public bool Bad
+        {
+            get => _Bad;
+            set
+            {
+                _Bad = value;
+                OnRaisePropertyChanged(nameof(Bad));
+            }
+        }
+
+        public int Age
+        {
+            get => _Age;
+            set
+            {
+                _Age = value;
+                OnRaisePropertyChanged(nameof(Age));
+            }
+        }
+        #endregion
+    }
+}
