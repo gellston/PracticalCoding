893b0537ca5af6d17751ca9d6feffaf99f0b3edd
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-06-05 21:57:04 +0900

    update animation example

---
+++ Source/C#/WPFAnimationExample/WPFAnimationExample/Model.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAnimationExample
+{
+    public class Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        #region Private Property
+        private bool _IsChecked = false;
+        #endregion
+
+        #region Public Property
+        public bool IsChecked
+        {
+            get => _IsChecked;
+            set
+            {
+                _IsChecked = value;
+                OnRaisePropertyChanged(nameof(IsChecked));
+            }
+  
+        }
+        #endregion
+
+        #region Function
+        public void OnRaisePropertyChanged(string name = "")
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+    }
+}
