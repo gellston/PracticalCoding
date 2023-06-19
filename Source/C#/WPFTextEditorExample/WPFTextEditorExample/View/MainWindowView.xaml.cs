fdea1973d18033e21ddb2ba7c6ee07dc2eba4642
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-06-19 15:55:31 +0900

    WPFTextEditorExample update

--- Source/C#/WPFTextEditorExample/WPFTextEditorExample/View/MainWindowView.xaml.cs
+++ Source/C#/WPFTextEditorExample/WPFTextEditorExample/View/MainWindowView.xaml.cs
@@ -12,17 +12,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFTextEditorExample.ViewModel;
 
-namespace WPFTextEditorExample
+namespace WPFTextEditorExample.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindowView : Window
     {
-        public MainWindow()
+        public MainWindowView()
         {
             InitializeComponent();
+
         }
     }
 }
---
+++ Source/C#/WPFTextEditorExample/WPFTextEditorExample/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPFTextEditorExample.Command;
+
+namespace WPFTextEditorExample.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Private Property
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string _Content = "";
+        private string _FilePath = "";
+        #endregion
+
+        #region Constructor
+        public MainWindowViewModel() { 
+        
+        }
+        #endregion
+
+        #region Public Property
+        public void OnRaiseProperty(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        public string Content
+        {
+            get => _Content;
+            set
+            {
+                _Content = value;
+                OnRaiseProperty(nameof(Content));
+            }
+        }
+
+        public string FilePath
+        {
+            get => _FilePath;
+            set
+            {
+                _FilePath = value;
+                OnRaiseProperty(nameof(FilePath));
+            }
+        }
+        #endregion
+
+        #region Command
+        public ICommand OpenFileCommand
+        {
+            get => new RelayCommand((arg) =>
+            {
+                try
+                {
+
+                    OpenFileDialog openFileDialog = new OpenFileDialog();
+                    openFileDialog.Filter = "Text files (*.txt)|*.txt";
+                    openFileDialog.Multiselect = false;
+                    if (openFileDialog.ShowDialog() == true)
+                    {
+
+                        var filePath = openFileDialog.FileName;
+                        var fileName = Path.GetFileName(filePath);
+                        this.FilePath = filePath;
+                        this.Content = File.ReadAllText(filePath);
+                        MessageBox.Show("성공적으로 로드되었습니다.");
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            });
+        }
+
+        public ICommand SaveFileCommand
+        {
+            get => new RelayCommand((arg) =>
+            {
+                try
+                {
+                    if(File.Exists(this.FilePath) == true)
+                    {
+
+
+                        File.WriteAllText(this.FilePath, this.Content);
+                        MessageBox.Show("성공적으로 저장되었습니다.");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            });
+        }
+        #endregion
+
+
+    }
+}
