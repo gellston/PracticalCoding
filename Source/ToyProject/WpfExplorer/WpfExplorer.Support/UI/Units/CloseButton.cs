a8f95f23b88859cc835d1cee4dfeb5eb204a1131
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2023-11-20 18:19:53 +0900

    1. wpf explorer 예제 토이 프로젝트 업데이트

---
+++ Source/ToyProject/WpfExplorer/WpfExplorer.Support/UI/Units/CloseButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfExplorer.Support.UI.Units
+{
+    
+    public class CloseButton : Button
+    {
+        static CloseButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CloseButton), new FrameworkPropertyMetadata(typeof(CloseButton)));
+        }
+    }
+}
---
+++ Source/ToyProject/WpfExplorer/WpfExplorer/App.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfExplorer
+{
+    internal class App : PrismApplication
+    {
+
+        protected override Window CreateShell()
+        {
+           return new Window();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
